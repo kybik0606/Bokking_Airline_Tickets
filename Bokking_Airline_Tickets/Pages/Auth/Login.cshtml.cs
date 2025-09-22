ade7d2463624f5eea414d1176e73742e2bfa6927
misha <misasklankov@gmail.com>
2025-09-22 12:53:45 +0300

    Добавил страницу авторизации , на кнопке войти. В  дальнейшем уже надо будет  подключать SQL БД, что бы мы могли сохранять пользователей. Фото на фоне , на странцие авторизации на рандом поставил , можно поменять на что нибудь более актуальное.

---
+++ Bokking_Airline_Tickets/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bokking_Airline_Tickets.Pages.Auth
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        public string Username { get; set; } = string.Empty;
+
+        [BindProperty]
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            // ⚡ Здесь будет логика проверки пользователя
+            if (Username == "admin" && Password == "123")
+            {
+                // Временно редиректим на главную
+                return RedirectToPage("/Index");
+            }
+
+            ModelState.AddModelError(string.Empty, "Неверное имя пользователя или пароль");
+            return Page();
+        }
+    }
+}
